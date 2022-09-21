45511330f75cbc90465ec8e62386e1929e423986
Luca Coduri <coduri.vitor@gmail.com>
2022-09-21 18:32:14 +0200

    script run

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var GithubWebHook = require("express-github-webhook");
 var webhookHandler = GithubWebHook({ path: "/webhook", secret: "secret" });
+const { exec } = require("child_process");
 
 // use in your express app
 let app = express();
@@ -9,16 +10,19 @@ app.use(bodyParser.json()); // must use bodyParser in express
 app.use(webhookHandler); // use our middleware
 
 // Now could handle following events
-webhookHandler.on("*", function (event, repo, data) {
+webhookHandler.on("*", async function (event, repo, data) {
     console.log("Received a webhook event:", event, "for", repo);
-    console.log(data);
+    if (repo == "HEIG-API") {
+        await new Promise((resolve, reject) => {
+            exec("sh script.sh", (error, stdout, stderr) => {
+                if (error !== null) {
+                    reject(error);
+                }
+                resolve();
+            });
+        });
+    }
 });
-
-// webhookHandler.on("event", function (repo, data) {});
-
-// webhookHandler.on("reponame", function (event, data) {});
-
-// webhookHandler.on("error", function (err, req, res) {});
 
 app.listen(3002, () => {
     console.log(`webhook listening on port ${3002}`);
