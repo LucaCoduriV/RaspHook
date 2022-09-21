a1603d45d2c32b24331b119714c7777c96f6bb4b
Luca Coduri <coduri.vitor@gmail.com>
2022-09-21 16:02:41 +0200

    fix

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ app.use(bodyParser.json()); // must use bodyParser in express
 app.use(webhookHandler); // use our middleware
 
 // Now could handle following events
-webhookHandler.on("*", function (event, repo, data) {});
+webhookHandler.on("*", function (event, repo, data) {
+    console.log("Received a webhook event:", event, "for", repo);
+});
 
-webhookHandler.on("event", function (repo, data) {});
+// webhookHandler.on("event", function (repo, data) {});
 
-webhookHandler.on("reponame", function (event, data) {});
+// webhookHandler.on("reponame", function (event, data) {});
 
-webhookHandler.on("error", function (err, req, res) {});
+// webhookHandler.on("error", function (err, req, res) {});
 
 app.listen(80, () => {
     console.log(`webhook listening on port ${port}`);
